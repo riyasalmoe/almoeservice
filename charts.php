71fd6178b9edd62c5e95d4d98304024598107955
riyas <riyas.a@ADS.LOCAL>
2018-06-13 13:20:13 +0400

    charts update with month-year summary

--- charts.php
+++ charts.php
@@ -3,6 +3,7 @@
     $con = dbconn();
     $GeoReport = "select * from view_yearly_jobcards_geo";
     $GlennReport = "select * from view_yearly_jobcards_glenn";
+    $YearMonthChart = "select * from view_all_jobcards_monthyear";
 ?>
 
 <!DOCTYPE html>
@@ -38,26 +39,44 @@
   </div>
 </nav>
 
-    <div class="row">
-        <div class="col-lg-3"></div>
-        <div class="col-lg-3">
-            <canvas id="myChartGeo" width="100" height="100"></canvas>
+    <div class="container-fluid">
+
+    <div class="row"> <!-- Row 1 --> 
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
+            <canvas id="YearMonthChart1" width="150" height="50"></canvas>
         </div>
-        <div class="col-lg-3">
-            <canvas id="myChartGlenn" width="100" height="100"></canvas>
+        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 text-center">
+            <canvas id="YearMonthChart2" width="150" height="50"></canvas>
         </div>
-        <div class="col-lg-3"></div>
-    </div>
-    <div class="row">
-        <div class="col-lg-3"></div>
-        <div class="col-lg-3">
-            <canvas id="myChartVijith" width="100" height="100"></canvas>
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
+            <canvas id="YearMonthChart3" width="150" height="50"></canvas>
         </div>
-        <div class="col-lg-3">
-            <canvas id="myChartShijo" width="100" height="100"></canvas>
+    <!-- view_all_jobcards_monthyear -->
+    </div> <!-- Row 1 -->
+
+    <div class="row"> <!-- Row 2 -->
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
+            <canvas id="GeoChart1" width="150" height="150"></canvas>
         </div>
-        <div class="col-lg-3"></div>
-    </div>
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
+            <canvas id="GlennChart2" width="150" height="150"></canvas>
+        </div>
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
+            <canvas id="chromisChart3" width="150" height="150"></canvas>Engineer 3
+        </div>
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
+            <canvas id="chromisChart4" width="100" height="100"></canvas> Engineer 4
+        </div>
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
+            <canvas id="chromisChart5" width="100" height="100"></canvas> Engineer 5
+        </div>
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 text-center">
+            <canvas id="chromisChart6" width="100" height="100"></canvas> Engineer 6
+        </div>
+    </div> <!-- Row 2 -->
+
+</div> <!-- container -->
+
 
     <?php //for Geo
                     $outputJobs = array();
@@ -80,7 +99,7 @@
     ?>
 
     <script>
-        var ctx = document.getElementById('myChartGeo').getContext('2d');
+        var ctx = document.getElementById('GeoChart1').getContext('2d');
         var chart = new Chart(ctx, {
             // The type of chart we want to create
             type: 'bar',
@@ -120,7 +139,7 @@
                                                    
     ?>
 <script>
-         var ctx = document.getElementById('myChartGlenn').getContext('2d');
+         var ctx = document.getElementById('GlennChart2').getContext('2d');
          var chart = new Chart(ctx, {
             // The type of chart we want to create
             type: 'bar',
@@ -140,47 +159,46 @@
             options: {}
         });
 </script>
-<script>     
-         var ctx = document.getElementById('myChartVijith').getContext('2d');
-         var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
 
-            // The data for our dataset
-            data: {
-                labels: ["2017", "2018"],
-                datasets: [{
-                    label: "Vijith",
-                    backgroundColor: 'rgb(156,39,176)',
-                    borderColor: 'rgb(156,39,176)',
-                    data: [0, 0],
-                }]
-            },
+<?php //for YearMonth Chart 2
 
-            // Configuration options go here
-            options: {}
-        });
+if ($stmt = $con->prepare($YearMonthChart)) {
+    $stmt->execute();
+    $stmt->bind_result($Jobs, $MonthYear);
+    $oYears = array();
+    $oJobs = array();
+    while ($stmt->fetch()) {
+        $oJobs[] = array($Jobs);  
+        $oYears[] = array($MonthYear);
+}
+    //print_r($ojobs);
+    //print(json_encode($oJobs));
+
+    $stmt->close();
+} 
+                               
+?>
+<script>
+    var ctx = document.getElementById('YearMonthChart2').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+    labels: <?php echo json_encode($oYears);?>,
+    datasets: [{
+    label: "Monthly Jobs",
+    backgroundColor: 'rgb(2,136,209)',
+    borderColor: 'rgb(2,136,209)',
+    data: <?php echo json_encode($oJobs);?>,
+    }]
+    },
+
+    // Configuration options go here
+    options: {}
+    });
 </script>
-<script>     
-         var ctx = document.getElementById('myChartShijo').getContext('2d');
-         var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'bar',
 
-            // The data for our dataset
-            data: {
-                labels: ["2017", "2018"],
-                datasets: [{
-                    label: "Shijo",
-                    backgroundColor: 'rgb(156,39,176)',
-                    borderColor: 'rgb(156,39,176)',
-                    data: [0, 0],
-                }]
-            },
-
-            // Configuration options go here
-            options: {}
-        });
-</script>
 </body>
 </html>
